620373d3f5dbacd1b7e4288c4fd34b2cdfa8944e
brake <riaconoo@icloud.com>
2022-05-29 15:00:39 +1000

    starter code commented, and plan layed out

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //variable is targeting the generate id within the HTML.
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); //Generate password is not a function.
+  var passwordText = document.querySelector("#password"); //#password refers to the card created within the HTML. Stores the generated password.
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //When user clicks, writePassword function is called.

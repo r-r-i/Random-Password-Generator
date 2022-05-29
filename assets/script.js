656fda2fc14a3ad2c4d65334d9a24d7fc0a01d61
brake <riaconoo@icloud.com>
2022-05-29 23:36:03 +1000

    added functions that return random numbers/symbols/upper&lowercase letters, started building user prompts

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //variable is targeting the generate id within the HTML.
 
+var lengthMin=8;
+var lengthMax=128;
+
 function generatePassword(){
+    var userChoice= window.prompt("How many characters would you like your password to contain?");
+
+    if (userChoice < lengthMin || userChoice > lengthMax) {
+        window.prompt("your password must contain between 8 and 128 characters"); 
+
+    } else if (userChoice > lengthMin || userChoice < lengthMax) {
+        window.confirm("Would you like your password to contain lower case letters?")
+    } else {
+        window.confirm("Would you like your password to contain upper case letters?")
+    }
+
+}
+    
+    
+    
+    
+        
+    
+
+
+
+    
 
 // 1. prompt the user for password criteria
 //    a. password length 8-128
@@ -12,7 +37,9 @@ function generatePassword(){
 
 
 
-}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -25,3 +52,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //When user clicks, writePassword function is called.
+
+
+
+function getRandomNumber(){
+    var numbers = ['0','1', '2','3','4','5','6','7','8','9'];
+    return numbers[Math.floor(Math.random()* numbers.length)]
+}
+
+function getRandomLower(){
+    var lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    return lowers[Math.floor(Math.random()* lowers.length)]
+}
+
+function getRandomUpper(){
+    var uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    return uppers[Math.floor(Math.random()* uppers.length)]
+    
+}
+function getRandomSymbol(){
+    var symbols = [ '@', '%', '+', '\\', '/', "'", '!','#','$','^', '?', ':', ',', ')', '(', '}', '{', ']','[', '~','-','_','.'];
+    return symbols[Math.floor(Math.random()* symbols.length)]
+}
+
+console.log(getRandomSymbol() + getRandomLower() + getRandomNumber() + getRandomUpper());
+
+
+
+

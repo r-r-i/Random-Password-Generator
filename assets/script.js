c84598d9e98484efb2029e6902029715586f159e
brake <riaconoo@icloud.com>
2022-05-29 15:16:37 +1000

    pseudo code added, plan updated

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //variable is targeting the generate id within the HTML.
 
+function generatePassword(){
+
+// 1. prompt the user for password criteria
+//    a. password length 8-128
+//    b. lowercase, uppercase, numeric, and/or special characters
+// 2. validate the user's input
+// 3. password is generated based on criteria
+// 4. display password to page
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); //Generate password is not a function.
